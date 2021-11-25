6b15411f8248737c83a562d24a21cd1a1348fabb
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-26 01:21:47 +0200

    generator works :yay:

--- src/songGenerator.ts
+++ src/songGenerator.ts
@@ -6,11 +6,14 @@ import { ICommonTagsResult } from "music-metadata/lib/type";
 const SONG_PATH = "./public/songs";
 
 async function writeJson(filename: string, data: ICommonTagsResult) {
+  const jsonPath =
+    filename
+      .substring(filename.lastIndexOf(" - ") + 3, filename.lastIndexOf("."))
+      .replace(/\s/g, "_")
+      .toLowerCase() + ".json";
+
   fs.writeFileSync(
-    `./src/assets/songs/${filename
-      .substr(filename.lastIndexOf(" - "), filename.lastIndexOf("."))
-      .replace(" ", "_")
-      .toLowerCase()}`,
+    `./src/assets/songs/${jsonPath}`,
     JSON.stringify(
       {
         artists: data.artists,
@@ -24,13 +27,14 @@ async function writeJson(filename: string, data: ICommonTagsResult) {
       2
     )
   );
+
+  console.log(`Generated JSON ${jsonPath} for ${filename}`);
 }
 
 async function main() {
-  console.log("Generating release templates");
+  console.log("Generating release templates\n");
 
   const songs = fs.readdirSync(SONG_PATH);
-  console.log(songs);
 
   for (let song of songs) {
     const buffer = fs.readFileSync(`${SONG_PATH}/${song}`);
