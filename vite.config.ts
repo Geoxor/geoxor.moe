86673429b250a85dba2f41350da69af4f31af95b
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-25 06:33:31 +0200

    add more base deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import WindiCSS from "vite-plugin-windicss";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
+import { FileSystemIconLoader } from "unplugin-icons/loaders";
+import ViteFonts from "vite-plugin-fonts";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  resolve: {
+    alias: {
+      "~/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
+  plugins: [
+    Components({
+      resolvers: [
+        IconsResolver({
+          customCollections: ["fluency"],
+        }),
+      ],
+    }),
+    WindiCSS(),
+    ViteFonts({
+      google: {
+        families: ["Roboto"],
+      },
+    }),
+    Icons({
+      customCollections: {
+        fluency: FileSystemIconLoader("./public/icons/svg"),
+      },
+    }),
+    vue(),
+  ],
+});
