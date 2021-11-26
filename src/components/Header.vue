4470ff570d5c9b56dabca3f3cba6d488a5997ba4
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-26 06:57:08 +0200

    noscript tag
    Co-Authored-By: Blusk <52386117+Bluskript@users.noreply.github.com>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,35 @@
 <template>
-  <header class="flex padding py-4 items-center justify-between">
-    <img class="logo h-4" src="../assets/geoxor_logo.svg" />
-    <ul class="flex w-full justify-end text-theme-800 gap-4 items-center uppercase text-sm">
-      <SocialMedia title="instagram" link="https://instagram.com/geoxor" />
-      <SocialMedia title="youtube" link="https://www.youtube.com/geoxor" />
-      <SocialMedia title="twitter" link="https://twitter.com/Geoxor" />
-      <SocialMedia title="tank" link="https://geoxor.newgrounds.com" />
-      <SocialMedia title="cloud" link="https://soundcloud.com/geoxor" />
-      <SocialMedia title="discord" link="https://discord.gg/geoxor" />
-      <SocialMedia title="github" link="https://github.com/Geoxor" />
-      <li>
-        <a target="_blank" href="https://store.geoxor.moe">Merch</a>
-      </li>
-      <li>
-        <router-link :to="{ name: 'minecraft' }">Minecraft</router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'downloads' }">Downloads</router-link>
-      </li>
-      <button
-        class="flex items-center text-theme-800"
-        @click="themeState === 'dark' ? (themeState = 'light') : (themeState = 'dark')"
-      >
-        <i-fluency-moon />
-      </button>
-    </ul>
+  <header class="padding py-4 flex flex-col">
+    <noscript class="bg-red-600 text-sm py-1 px-2 w-full rounded-4px mb-2"
+      >This site works better with Javascript enabled</noscript
+    >
+    <div class="flex items-center justify-between">
+      <img class="logo h-4" src="../assets/geoxor_logo.svg" />
+      <ul class="flex w-full justify-end text-theme-800 gap-4 items-center uppercase text-sm">
+        <SocialMedia title="instagram" link="https://instagram.com/geoxor" />
+        <SocialMedia title="youtube" link="https://www.youtube.com/geoxor" />
+        <SocialMedia title="twitter" link="https://twitter.com/Geoxor" />
+        <SocialMedia title="tank" link="https://geoxor.newgrounds.com" />
+        <SocialMedia title="cloud" link="https://soundcloud.com/geoxor" />
+        <SocialMedia title="discord" link="https://discord.gg/geoxor" />
+        <SocialMedia title="github" link="https://github.com/Geoxor" />
+        <li>
+          <a target="_blank" href="https://store.geoxor.moe">Merch</a>
+        </li>
+        <li>
+          <router-link :to="{ name: 'minecraft' }">Minecraft</router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'downloads' }">Downloads</router-link>
+        </li>
+        <button
+          class="flex items-center text-theme-800"
+          @click="themeState === 'dark' ? (themeState = 'light') : (themeState = 'dark')"
+        >
+          <i-fluency-moon />
+        </button>
+      </ul>
+    </div>
   </header>
 </template>
 
