0f69887e77812656ec0d3015ed9f174de578c27d
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-27 00:08:53 +0200

    fix theming not applying first time
    Co-Authored-By: Blusk <52386117+Bluskript@users.noreply.github.com>

--- src/logic.ts
+++ src/logic.ts
@@ -5,7 +5,7 @@ if (typeof window !== "undefined") {
   link.type = "text/css";
   link.rel = "stylesheet";
   link.media = "screen,print";
-  link.href = `/themes/${localStorage.getItem("theme")}.css`;
+  link.href = `/themes/${localStorage.getItem("theme") || "dark"}.css`;
   document.getElementsByTagName("head")[0].appendChild(link);
 }
 
