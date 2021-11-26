1b0a48c5fb7a7d148d465b411cbd4c75312de78d
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-26 09:23:03 +0200

    redirect to downloads (for now)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ViteSSG } from "vite-ssg";
 
 export const createApp = ViteSSG(App, { routes }, ({ app, router, routes, isClient, initialState }) => {
   router.beforeEach((to, from, next) => {
+    if (to.name === "home") return next({ name: "downloads", params: { category: "songs" } });
+
     // Redirect if the user somehow entered /downloads with no category selected /downloads/:song
     if (to.name === "downloads" && !to.params.category) return next({ name: to.name, params: { category: "songs" } });
     return next();
