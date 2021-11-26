0224867dcb0f6548c56f84d12c9ac8f1e1ee13d6
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-26 04:48:06 +0200

    impl blusk's bullshit css helper
    Co-Authored-By: Blusk <52386117+Bluskript@users.noreply.github.com>

--- windi.config.ts
+++ windi.config.ts
@@ -2,24 +2,36 @@ import { defineConfig } from "vite-plugin-windicss";
 import typography from "windicss/plugin/typography";
 import colors from "windicss/colors";
 
+function cssVarRgbHelper(cssVariable: string) {
+  return ({ opacityVariable, opacityValue }: { opacityVariable: string; opacityValue: number }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(--${cssVariable}), ${opacityValue})`;
+    }
+    if (opacityVariable !== undefined) {
+      return `rgba(var(--${cssVariable}), var(${opacityVariable}, 1))`;
+    }
+    return `rgb(var(--${cssVariable}))`;
+  };
+}
+
 export default defineConfig({
   darkMode: "class",
   plugins: [typography()],
   theme: {
     extend: {
-      ...colors,
       colors: {
-        accent: "var(--accent-color)",
+        ...colors,
+        accent: cssVarRgbHelper("accent-color"),
         theme: {
-          100: "var(--theme-100)",
-          200: "var(--theme-200)",
-          300: "var(--theme-300)",
-          400: "var(--theme-400)",
-          500: "var(--theme-500)",
-          600: "var(--theme-600)",
-          700: "var(--theme-700)",
-          800: "var(--theme-800)",
-          900: "var(--theme-900)",
+          100: cssVarRgbHelper("theme-100"),
+          200: cssVarRgbHelper("theme-200"),
+          300: cssVarRgbHelper("theme-300"),
+          400: cssVarRgbHelper("theme-400"),
+          500: cssVarRgbHelper("theme-500"),
+          600: cssVarRgbHelper("theme-600"),
+          700: cssVarRgbHelper("theme-700"),
+          800: cssVarRgbHelper("theme-800"),
+          900: cssVarRgbHelper("theme-900"),
         },
       },
     },
