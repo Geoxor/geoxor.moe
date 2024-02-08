b2a97d905dc0f92c65692377b38875c6efd9153e
Geoxor <34042825+Geoxor@users.noreply.github.com>
2024-02-08 17:17:32 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ export const createApp = ViteSSG(App, { routes }, ({ app, router, routes, isClie
   router.beforeEach((to, from, next) => {
     // Redirect if the user somehow entered /downloads with no category selected /downloads/:song
     if (to.name === "downloads" && !to.params.category) return next({ name: to.name, params: { category: "songs" } });
-    return next();
+    return next(); 
   });
 });
